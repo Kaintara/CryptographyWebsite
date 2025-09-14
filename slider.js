941850fdc871c532bab382db8e838df71e516d1d
Kaintara <barli2k@gmail.com>
2025-09-14 12:48:32 +0100

    Finished up the intro of the webpage

--- slider.js
+++ slider.js
@@ -2,7 +2,15 @@ const slider = document.getElementById('slider')
 const sliderValue = document.getElementById('sliderValue')
 const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M","N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+const alphabetObj = Array.from(document.querySelectorAll('.letter'))
+
+const alphabetObjOrigin = alphabetObj.map(el => el.textContent.trim())
+
 slider.addEventListener('input',function() {
     sliderValue.textContent = `Shift Key: ${slider.value}`
-
+    const shiftKey = Number(slider.value)
+    alphabetObj.forEach((element, i) => {
+        const index = alphabet.indexOf(alphabetObjOrigin[i]);
+        element.textContent = alphabet[(index + shiftKey) % 26]
+});
 })
