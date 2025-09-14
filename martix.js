9c71e5b6d834941eeff79c947e16143b80dcee61
Kaintara <barli2k@gmail.com>
2025-09-14 10:18:32 +0100

    Added the letter animation.

--- martix.js
+++ martix.js
@@ -1,17 +1,34 @@
 const canvas = document.getElementById("mat")
+canvas.width = window.innerWidth
+canvas.height = 200
 const context = canvas.getContext('2d')
 
 const letters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッンABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-const fontSize = 10;
+const fontSize = 16;
 const columns = canvas.width/fontSize;
 
 const letcol = [];
 
-for (let i=0; x < columns; x++) {
+for (let i=0; i < columns; i++) {
     letcol[i] = 1
 }
 
 const draw = () => {
     context.fillStyle = 'rgba(0,0,0,0.5)'
-    context.fillRext(0,0,canvas.width, canvas.height)
-}
+    context.fillRect(0,0,canvas.width, canvas.height)
+
+    context.fillStyle = '#0239cfff';
+    context.font = fontSize + 'px Iceland, monospace';
+
+    for (let i=0; i < letcol.length; i++) {
+        const letter = letters.charAt(Math.floor(Math.random()*letters.length));
+        context.fillText(letter, i*fontSize, letcol[i]*fontSize)
+
+        if (letcol[i]*fontSize > canvas.height && Math.random() > 0.9) {
+            letcol[i] = 0;
+        }
+        letcol[i]++;
+    }
+};
+
+setInterval(draw,40);
