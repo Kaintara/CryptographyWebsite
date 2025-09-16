496134177c5ceff7a25c1ebd1419f1127d993359
Kaintara <barli2k@gmail.com>
2025-09-16 21:52:40 +0100

    Default example has been written out.

---
+++ DHKE.js
@@ -0,0 +1,13 @@
+const x = document.getElementById('x')
+const y = document.getElementById('y')
+const b = document.getElementById('b')
+const m  = document.getElementById('m')
+
+const xtext = document.getElementById('xtext')
+const ytext = document.getElementById('ytext')
+const btext = document.getElementById('btext')
+const mtext  = document.getElementById('mtext')
+
+const resultA = document.getElementById('resultA')
+const resultB = document.getElementById('resultB')
+const resultC = document.getElementById('resultC')
