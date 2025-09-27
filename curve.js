e9c4cdf0eec06bf7139a172ccc63d2c4d037c12f
Kaintara <barli2k@gmail.com>
2025-09-27 23:42:15 +0100

    Managed to generate a elliptic curve using Chart.js!

--- curve.js
+++ curve.js
@@ -1,24 +1,57 @@
 const curve = document.getElementById('curve')
-const a = document.getElementById("a")
-const b = document.getElementById("b")
+const a = document.getElementById("a_input")
+const b = document.getElementById("b_input")
 
-const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+let curveChart = null
 
-drawCurve(xValues,yValues)
+drawCurve()
 
-function drawCurve(xValues, yValues) {
+function drawCurve() {
+    const points = generatePoints()
+    const full = points[0].concat(points[1])
+    if (curveChart) {
+        curveChart.destroy()
+    }
     curveChart = new Chart(curve, {
-        type: "line",
+        type: "scatter",
         data: {
-            labels: xValues,
             datasets: [{
-                data:yValues
-                
+                label: "Ellipric Curve",
+                data: full,
+                color: "#0239cfff",
+                borderColor: "#0239cfff",
+                backgroundColor: "#0239cfff",
+                font: {
+                    family:'Iceland',
+                }
+
             }]
         },
         options: {
-            //responsive: false,
+            scales: {
+                x: {min: -20, max: 20},
+                y: {min: -50, max: 50}
+            }
         }
     })
 }
+
+function generatePoints() {
+    const pos_points = [];
+    const neg_points = []
+    for (let i = -20; i <= 20; i+= 0.001){
+        const y_squared = i**3 + Number(a.value)*i + Number(b.value)
+        if (y_squared >= 0) {
+            const y = Math.sqrt(y_squared)
+            pos_points.push({x: i,y: y})
+            neg_points.push({x: i,y: -y})
+        }}
+    return [pos_points, neg_points]
+}
+
+
+a.addEventListener('keydown', (eve) => {
+    if (eve.key === 'Enter') {
+        drawCurve()
+    }
+})
