82f2c9c9753d91c3e6b1b89a8e0b454584b96b16
Kaintara <barli2k@gmail.com>
2025-09-28 00:22:26 +0100

    Fixed bug where the graph would not shrink to adjust to the size of the page.

--- curve.js
+++ curve.js
@@ -1,3 +1,7 @@
+Chart.defaults.backgroundColor = "#0239cfff";
+Chart.defaults.borderColor = "#0239cfff";
+Chart.defaults.color = "#0239cfff";
+
 const curve = document.getElementById('curve')
 const a = document.getElementById("a_input")
 const b = document.getElementById("b_input")
@@ -16,30 +20,36 @@ function drawCurve() {
         type: "scatter",
         data: {
             datasets: [{
-                label: "Ellipric Curve",
                 data: full,
-                color: "#0239cfff",
-                borderColor: "#0239cfff",
-                backgroundColor: "#0239cfff",
-                font: {
-                    family:'Iceland',
-                }
-
+                pointRadius: 1,
             }]
         },
         options: {
             scales: {
                 x: {min: -20, max: 20},
                 y: {min: -50, max: 50}
+            },
+            plugins: {
+                tooltip: {
+                    enabled: false // disables tooltips on hover
+                },
+                legend: {
+                    display: false,
+                }
+            },
+            hover: {
+                mode: null // disables hover interactions
+            },
+            responsive: true,
+            maintainAspectRatio: false,
             }
-        }
     })
 }
 
 function generatePoints() {
     const pos_points = [];
     const neg_points = []
-    for (let i = -20; i <= 20; i+= 0.001){
+    for (let i = -20; i <= 20; i+= 0.005){
         const y_squared = i**3 + Number(a.value)*i + Number(b.value)
         if (y_squared >= 0) {
             const y = Math.sqrt(y_squared)
@@ -51,6 +61,12 @@ function generatePoints() {
 
 
 a.addEventListener('keydown', (eve) => {
+    if (eve.key === 'Enter') {
+        drawCurve()
+    }
+})
+
+b.addEventListener('keydown', (eve) => {
     if (eve.key === 'Enter') {
         drawCurve()
     }
