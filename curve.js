67c3c6e036a379d685c768f4d42b857ac309ea80
Kaintara <barli2k@gmail.com>
2025-09-27 22:38:54 +0100

    Fixed bug where the chart drawn on canvas would not stop expanding.

--- curve.js
+++ curve.js
@@ -1,9 +1,24 @@
 const curve = document.getElementById('curve')
-const ctxx = curve.getContext('2d')
+const a = document.getElementById("a")
+const b = document.getElementById("b")
 
-const myChart = new Chart(ctxx, {
-    type:"scatter",
-    data: {
-        
-    }
-})
+const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
+drawCurve(xValues,yValues)
+
+function drawCurve(xValues, yValues) {
+    curveChart = new Chart(curve, {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                data:yValues
+                
+            }]
+        },
+        options: {
+            //responsive: false,
+        }
+    })
+}
