73230334d038437f0e26201e56078c613c8b5282
Kaintara <barli2k@gmail.com>
2025-09-28 05:33:16 +0100

    Completed the project!!!

--- finite.js
+++ finite.js
@@ -9,6 +9,10 @@ let elliptifield = null;
 function mod(n, p=2467) {
   return ((n % p) + p) % p;
 }
+
+function modINT(n, p=2467n) {
+  return ((n % p) + p) % p;
+}
 function genpoints(a=-1, b=1, p=2467) {
     const points = []
      for (let x = 0; x < p; x++) {
@@ -30,7 +34,7 @@ function drawfield() {
         data: {
             datasets: [{
                 data: points,
-                pointRadius: 0.5,
+                pointRadius: 2,
                 showLine: false
             }]
         },
@@ -57,6 +61,75 @@ function drawfield() {
     })
 }
 
+function Euclidean(a, b) {
+  a = BigInt(a); b = BigInt(b);
+  let x0 = 1n, x1 = 0n;
+  let y0 = 0n, y1 = 1n;
+  while (b != 0n) {
+    const q = a / b;
+    const r = a % b;
+    [a, b] = [b, r];
+    [x0, x1] = [x1, x0 - q * x1];
+    [y0, y1] = [y1, y0 - q * y1];
+  }
+  return { g: a, x: x0, y: y0 };
+}
+
+
+function isInfinity(P) { return P === null; }
+function pointNeg(P) {
+  if (P === null) return null;
+  return { x: P.x, y: modINT(-P.y) };
+}
+
+function modInv(n, m = 2467n) {
+  const { g, x } = Euclidean(modINT(n, m), m);
+  if (g !== 1n) throw new Error('modular inverse does not exist');
+  return modINT(x, m);
+}
+
+function pointAdd(P,Q) {
+    if (P === null) return Q;
+    if (Q === null) return P;
+
+  if (P.x === Q.x && modINT(P.y + Q.y) === 0n) return null;
+
+  let m;
+  if (P.x === Q.x && P.y === Q.y) {
+ 
+    const num = modINT(3n * P.x * P.x -1n);
+    const den = modInv(2n * P.y);
+    m = modINT(num * den);
+  } else {
+ 
+    const num = modINT(Q.y - P.y);
+    const den = modInv(Q.x - P.x);
+    m = modINT(num * den);
+  }
+
+  const xr = modINT(m * m - P.x - Q.x);
+  const yr = modINT(m * (P.x - xr) - P.y);
+  return { x: xr, y: yr };
+}
+
+
+function genPublic(G,d) {
+    d = BigInt(d)
+    if (d === 0n || G === null) return null;
+
+    let Q = null
+    let R = G
+
+    while (d > 0n) {
+        if (d & 1n) {
+            Q = pointAdd(Q,R);
+        }
+        R = pointAdd(R,R)
+        d >>= 1n
+    }
+    return Q
+}
+
 ff.addEventListener("click", (eve) => {
     const points = elliptifield.getElementsAtEventForMode(eve, 'nearest', {intersect: true}, true);
     if (points.length) {
@@ -64,6 +137,10 @@ ff.addEventListener("click", (eve) => {
         if (datasetIndex > 1) return
         const selected = elliptifield.data.datasets[datasetIndex].data[index];
         gentext.textContent = `${selected.x},${selected.y}`
+        const d = BigInt(privkey.value)
+        const G = {x:BigInt(selected.x), y:BigInt(selected.y)}
+        const Q = genPublic(G,d)
+        publictext.textContent = `${Q.x},${Q.y}`
     }
 })
 
